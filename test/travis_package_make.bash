f02a5b0cb25cde076462ad652892fe1c9a15f502
Makoto Takeuchi <1143013@stu.hus.ac.jp>
2017-10-27 06:52:34 +0000

    wall_stop

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -5,7 +5,7 @@ rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 
 #clone pimouse_ros
 cd ~/catkin_ws/src/
-git clone --https://github.com/makototakeuchi0/pimouse_ros
+git clone --depth=1 https://github.com/makototakeuchi0/pimouse_ros
 
 cd ~/catkin_ws
 catkin_make
